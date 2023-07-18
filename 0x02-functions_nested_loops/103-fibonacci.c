f5b9b238fbc75c7c2b1e626317fe93a22e08b1b8
RetagFathiAs <retagfathi128@gmail.com>
2023-07-18 15:34:22 -0700

    c programming

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,7 +2,7 @@
 /**
 * main - finds and prints the sum of the even-valued terms
 * Description : followed by a new line
-* Return: Always 0 (Success)
+* Return: Always 0 "Success"
 */
 int main(void)
 {int i;
