614451f1c39fda63d38205cf65795fba82a67cd6
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-08 00:01:44 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array : functins creates an array of chars, 
+ * create_array : functins creates an array of chars,
  * and initializes it with a specific char.
  * description : create_array - using pointers
  *@size: size of the array
@@ -20,5 +20,5 @@ for (j = 0; j < size; j++)
 {
 arrays[j] = c;
 }
-return (arrays);
+       return (arrays);
 }
