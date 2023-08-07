b7fbd4233aefc701e49ad6e4b8d33d7f625f142b
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-07 23:58:16 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array : functins creates an array of chars, and initializes it with a specific char.
- *
- * Returns : NULL if size = 0, a pointer to the array, or NULL if it fails.
+ * create_array : functins creates an array of chars, 
+ * and initializes it with a specific char.
+ * description : create_array - using pointers
+ *@size: size of the array
+ *@c: character
+ * Return : NULL if size = 0, a pointer to the array, or NULL if it fails.
  */
 char *create_array(unsigned int size, char c)
 {
 char *arrays = malloc(size * sizeof(char));
-unsigned int j =0;
+unsigned int j = 0;
 if (size == 0)
 {
 return (NULL);
