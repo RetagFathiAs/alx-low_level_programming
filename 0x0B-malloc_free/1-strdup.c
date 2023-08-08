6288e3b4167359319e8e6114ed5c5f0cc5331a56
RetagFathiAs <retagfathi128@gmail.com>
2023-08-08 03:41:43 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "stdlib.h"
+#include <stdlib.h>
 /**
  *  _strdup - Entry point
  *@str: string we need to duplicate
@@ -8,17 +8,17 @@
 char *_strdup(char *str)
 {
 	char *strmall = NULL;
-	unsigned int k = 0;
+	int k = 0;
 	int counter;
 
 	if (str == NULL)
 		return (NULL);
 	for (counter = 0 ; str[counter] != '\0' ; counter++)
 		;
-strmall = (char *)malloc(counter + 1 * sizeof(char));
+strmall = (char *)malloc((sizeof(char) * counter) + 1);
 if (strmall != NULL)
 {
-	for (k = 1 ; str[k] != '\0' ; k++)
+	for (k = 1 ; k < counter ; k++)
 		strmall[k] = str[k];
 }
 else
