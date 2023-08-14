f9c61f2820b600232ca9bd9feb21196bab366b3f
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-15 01:04:55 +0200

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: new dog pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int nl, num, counter;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (counter = 0; counter < nl; counter++)
+		dog->name[counter] = name[counter];
+	dog->age = age;
+	for (num = 0; owner[num]; num++)
+		;
+	ol++;
+	dog->owner = malloc(num * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (counter = 0; counter < num; counter++)
+		dog->owner[counter] = owner[counter];
+	return (dog);
+}
