f6453d8a215bc9e889d029451fe620ca0ae4a080
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-18 02:48:05 +0200

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,24 +5,25 @@
  * sum_them_all - calculates the sum
  * @n: number of args
  * Return: 0,or the sum
+ *
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int count,sum;
+	unsigned int count, sum;
 	sum = 0;
-	va_start(ap,n);
+	va_start (ap, n);
 	if(n == 0)
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-	for (count = 0;count < n;count++)
+	for (count = 0; count < n; count++)
 	{
-		sum += va_arg(ap,unsigned int);
+		sum += va_arg(ap, unsigned int);
 	}
 	va_end(ap);
-	return sum;
+	return (sum);
 	}
 }
