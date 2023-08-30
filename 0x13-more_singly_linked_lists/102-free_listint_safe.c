1cff0f6b6edd4203a05ad20304c55f519ac79567
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-29 17:40:07 -0700

    alx

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,37 +1,62 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a list
- * @h: data
- * Return: the size
+ * _ra - reallocates memorynters
+ * to the nodes st
+ * @list: the ond
+ * @size: size o han the old list)
+ * @new: new nodelist
+ *
+ * Return: point w list
  */
-size_t free_listint_safe(listint_t **h)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	listint_t **newlist;
+	size_t i;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
-		}
+		free(list);
+		exit(98);
 	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
 
-	*h = NULL;
+/**
+ * free_listint_safe - frees 
+ * @head: double pointer to the
+ * Return: the nu the list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t counter, j = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	return (len);
+	if (head == NULL || *head == NULL)
+		return (counter);
+	while (*head != NULL)
+	{
+		for (j = 0; j < counter; j++)
+		{
+			if (*head == list[j])
+			{
+				*head = NULL;
+				free(list);
+				return (counter);
+			}
+		}
+		counter++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (counter);
 }
