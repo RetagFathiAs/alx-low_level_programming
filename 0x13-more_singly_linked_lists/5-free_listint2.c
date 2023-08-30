16547c1f7d024834b2b9f827a2cc43e44cc712d3
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-29 17:22:39 -0700

    alx

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,14 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *next;
+
 	if (head == NULL)
 		return;
-	while (*head)
+	while (*head != NULL)
 	{
+		next = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
+		*head = next;
 	}
-	head = NULL;
 }
