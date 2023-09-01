63a70c8654253ba99052cb73d3c6b3a2c6c5a435
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-31 19:07:03 -0700

    alx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int v = 0;
-	
+
 if (!b)
-return (0);
-while(*b)
-	{
-		if (*b != '0' && *b != '1')
-			return (0);
-		v = v * 2 + (*b++ - '0');
-	}
+	return (0);
+while (*b)
+{
+if (*b != '0' && != '1')
+	return (0);
+v = v * 2 + (*b++ - '0');
+}
 return (v);
 }
