6a21c46b233c0c8850dc29119b1cd55a54f6a7bd
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-08-31 18:15:00 -0700

    alx

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,18 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 1 at a given index
- * @n: type unsigned long int input pointer
- * @index: type unsigned int position of unit
- * Return: 1 if works -1 if not
+ * clear_bit - sets the va1 at a given index
+ * @n: type unsiinput pointer
+ * @index: type unn of unit
+ * Return: 1 if w not
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int m;
 
 	if (index > 63)
 		return (-1);
-	mask = 1 << index;
-	*n &= ~(mask);
+	m = 1 << index;
+	*n &= ~(m);
 	return (1);
 }
