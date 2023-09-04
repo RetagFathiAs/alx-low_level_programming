e03bc18f6179ec54e87d26299113cbda99165712
RetagFathiAs <retagfathi128@gmail.com>
2023-09-04 13:36:15 -0700

    alx

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	of = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (!of)
+	if (of == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		}
 		wf = write(of, text_content, counter);
 	}
-	if (!of && wf != counter)
+	if (wf == -1 && wf != counter)
 		return (-1);
 	return (1);
 }
