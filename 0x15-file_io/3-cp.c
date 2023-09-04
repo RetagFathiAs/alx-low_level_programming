31d16ed86d02d6b96dea046e948f494387678564
RetagFathiAs <retagfathi128@gmail.com>
2023-09-04 16:15:38 -0700

    alx

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,32 +3,32 @@
 #define BUFFER_SIZE 1024
 /**
  * close_w - close function
- * @fd: read
- * @fd: write
+ * @fr: read
+ * @fw: write
  */
-void close_w(int fd, int fd)
+void close_w(int fr, int fw)
 {
-	if (close(fd) == -1)
+	if (close(fw) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fw);
 		exit(100); }
-	if (close(fd) == -1)
+	if (close(fr) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fr);
 		exit(100); }
 }
 
 /**
- * main - Function that copienother file
- * @argc: argumenount
- * @argv: argrray
- * Return: exit_successr 97, 98, 99, 100
+ * main - Function that copieto another file
+ * @argc: argument of count
+ * @argv: argument of array
+ * Return: exit_success if or 97, 98, 99, 100
  */
 int main(int argc, char *argv[])
 {
 	char buffer[BUFFER_SIZE];
 	char *file_from, *file_to;
-	int fd, fd;
+	int fr, fw;
 	ssize_t rd = 1024, wr;
 
 	if (argc != 3)
@@ -37,29 +37,28 @@ int main(int argc, char *argv[])
 		exit(97); }
 	file_from = argv[1];
 	file_to = argv[2];
-	fd = open(file_from, O_RDONLY);
-	if (fd == -1)
+	fr = open(file_from, O_RDONLY);
+	if (fr == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98); }
-	fd = open(file_to, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
-	if (fd == -1)
+	fw = open(file_to, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
+	if (fw == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99); }
 	while (rd == BUFFER_SIZE)
 	{
-		rd = read(fd, buffer, BUFFER_SIZE);
+		rd = read(fr, buffer, BUFFER_SIZE);
 		if (rd == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98); }
-		wr = write(fd, buffer, rd);
+		wr = write(fw, buffer, rd);
 		if (wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99); }}
-	close_w(fd, fd);
+	close_w(fr, fw);
 	return (0);
 }
-
