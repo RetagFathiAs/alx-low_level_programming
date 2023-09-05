6f8036afdc1f3c268099984b33163f1200f15b9b
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-09-04 17:12:45 -0700

    alx

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,31 +1,50 @@
 #include "main.h"
 
 /**
+ * _strlen - compute the length
+ * @str: the string
+ *
+ * Return: the length
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
  * append_text_to_file - Function a file
  * @filename: type char filenam
  * @text_content: type char
  * Return: -1 if fail  1 on sucess
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int of = 0;
-	int wr = 0;
-	int counter;
+	ssize_t b_written = 0;
+	int fd;
 
-	of = open(filename, O_WRONLY | O_APPEND);
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (of == -1)
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd < 0)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-	while (*(text_content + counter) != '\0')
-	{
-		counter++;
-	}
-	wr = write(of, text_content, counter);
-	close(of);
-	if (wr == -1)
+
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+
+	if (b_written < 0)
 		return (-1);
 	return (1);
 }
