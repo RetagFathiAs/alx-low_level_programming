061e91e0f8d097e5f5ba7814a68a78c47e214221
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-12 21:08:06 +0200

    alx

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return the number of elements in dlistint
+ * @h: type dlistint pointer of nodes
+ * Return: n_nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t count = 0;
+        while (h)
+        {
+                h = h->next;
+                count++;
+        }
+        return (count);
+}
