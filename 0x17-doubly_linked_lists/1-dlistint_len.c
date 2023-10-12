31ca3945448ab2fc44686a4d62a252bbf59273fd
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-12 21:24:52 +0200

    alx

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,11 +8,11 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t count = 0;
-        while (h)
-        {
-                h = h->next;
-                count++;
-        }
-        return (count);
+size_t count = 0;
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
 }
