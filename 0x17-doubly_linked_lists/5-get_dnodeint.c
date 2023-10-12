a9abe229b5fe0b583d95deb2709b893df4295c23
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-12 22:22:24 +0200

    ALX

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get the node of a linked list
+ * @head: head of the list
+ * @index: index
+ * Return: data
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t count = 0;
+
+	while (head != NULL)
+	{
+		if (count == index)
+    {
+      return (NULL);
+    }
+		count++;
+		head = head->next;
+	}
+
+	return (head);
+}
