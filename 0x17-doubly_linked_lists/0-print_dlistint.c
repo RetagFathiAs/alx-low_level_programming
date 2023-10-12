07edb1e5d3aa2305db1f5c4882d75d7558c3fbe8
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2023-10-12 21:25:47 +0200

    alx

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,14 +5,15 @@
  * @h: type dlistint point of prev and next node
  * Return: n_nodes.
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-        size_t count = 0;
-        while (h)
-        {
-                printf("%d",h->n);
-                h = h->next;
-                count++;
-        }
-        return (count);
+size_t count = 0;
+while (h)
+{
+printf("%d",h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
