c8b55908b69a6fa77f30558f0fb2918de3f9998d
RetagFathiAs <retagfathi128@gmail.com>
2023-10-12 14:23:25 -0700

    alx

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,17 +8,16 @@
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	size_t count = 0;
+size_t count = 0;
 
-	while (head != NULL)
-	{
-		if (count == index)
-    {
-      return (NULL);
-    }
-		count++;
-		head = head->next;
-	}
-
-	return (head);
+while (head != NULL)
+{
+if (count == index)
+{
+return (NULL);
+}
+count++;
+head = head->next;
+}
+return (head);
 }
